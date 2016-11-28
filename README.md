b12e1bbbad647e3d37f44574732cdaac26262a58
Kaspars Andzans <kaspars_andzans@inbox.lv>
2016-11-28 12:42:27 +0000

    Update

---
+++ README.md
@@ -0,0 +1,5 @@
+FreeCodeCamp API Project: File Metadata Microservice.
+
+Created for learning purposes, with help from Theodore Anderson - "https://www.youtube.com/channel/UCFhC3Z6_9zjsxfBADZ-CEEg".
+
+Live app running on Heroku - "https://projectfmm.herokuapp.com/".
--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 var express = require("express");
 var app = express();
+var multer = require("multer");
+var upload = multer();
+
+app.get("/", function(req, res) {
+    res. sendFile(__dirname + "/index.html");
+});
+
+app.post("/upload", upload.single("file"), function(req, res){
+    if (req.file === undefined) {
+        res.send({"error":"No file selected"});
+    } else {
+    var ready = {
+        "file": req.file.originalname, 
+        "size": req.file.size
+        };
+    res.json(ready);
+    }
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port, function(){
